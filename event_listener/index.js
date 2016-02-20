60cdb7f4cdbe84eb295e868b6fa785d2706637af
okbp <yokookbio@gmail.com>
2016-02-21 04:32:10 +0900

    Update event_listener stanza

--- event_listener/index.js
+++ event_listener/index.js
@@ -9,6 +9,8 @@ Stanza(function(stanza, params) {
     stanza.render({
       template: "stanza.html",
     });
+    //プルダウンリストを初期値の値にセット
+    $(stanza.select('#current_value')).html(stanza.select('#select1').value);
 
     //use plain javascript
     stanza.select('#button1').addEventListener("click", function(e) {
@@ -17,7 +19,8 @@ Stanza(function(stanza, params) {
 
     //use jQuery 
     $(stanza.select('#select1')).on('change', function(e) {
-      alert(stanza.select('#select1').value);
+      $(stanza.select('#current_value')).empty();
+      $(stanza.select('#current_value')).html(stanza.select('#select1').value);
     });
   });
 });
